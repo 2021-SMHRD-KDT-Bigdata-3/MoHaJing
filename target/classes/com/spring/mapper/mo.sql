ee68c9bcbd926c2fb7184e161bdce7c4cb096024
김아름 <85213464+areum52@users.noreply.github.com>
2021-07-30 08:27:13 +0900

    DB테이블 생성

---
+++ target/classes/com/spring/mapper/mo.sql
@@ -0,0 +1,54 @@
+CREATE TABLE mo_member (
+id varchar(50) primary key,
+pw varchar(30) not null,
+name varchar(30) not null,
+gender varchar(10) not null,
+age int(5) not null,
+checknum int(5) not null
+);
+
+CREATE TABLE mo_board(
+no int not null auto_increment,
+id varchar(50),
+title varchar(50) not null,
+content varchar(1000) not null,
+file varchar(100),
+day datetime,
+primary key (no),
+foreign key (id) references mo_member (id)
+);
+
+CREATE TABLE mo_comment(
+no int not null,
+id varchar(50) not null,
+content varchar(1000) not null,
+day datetime,
+foreign key (no) references mo_board (no),
+foreign key (id) references mo_member (id)
+);
+
+CREATE TABLE mo_info(
+category varchar(30) not null,
+cause varchar(1000) not null,
+sign varchar(1000) not null,
+plan varchar(1000) not null
+);
+
+CREATE TABLE mo_recommand(
+category varchar(30) not null,
+food varchar(100) not null,
+plan varchar(1000) not null
+);
+
+CREATE TABLE mo_deep (
+id varchar(50),
+img varchar(100) not null,
+category varchar(30) not null,
+step varchar(30) not null,
+percent float(10) not null,
+date datetime,
+foreign key (id) references mo_member (id)
+);
+
+commit;
+
