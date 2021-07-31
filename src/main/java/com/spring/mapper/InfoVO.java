6967daace5860bff08546b4b4e594adc22fbfb0f
김아름 <85213464+areum52@users.noreply.github.com>
2021-07-31 16:13:31 +0900

    탈모정보

---
+++ src/main/java/com/spring/mapper/InfoVO.java
@@ -0,0 +1,13 @@
+package com.spring.mapper;
+
+import lombok.Data;
+
+@Data
+public class InfoVO {
+	
+	private String category;
+	private String cause;
+	private String sign;
+	private String plan;
+
+}
