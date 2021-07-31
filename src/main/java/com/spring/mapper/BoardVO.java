0b8abd5d557b4d6a717d0c22810506918abb1b7d
김아름 <85213464+areum52@users.noreply.github.com>
2021-07-31 11:21:31 +0900

    게시글보기, 게시글입력, 댓글보기, 댓글입력 백엔드

---
+++ src/main/java/com/spring/mapper/BoardVO.java
@@ -0,0 +1,12 @@
+package com.spring.mapper;
+import lombok.Data;
+@Data
+public class BoardVO {
+	private int no;
+	private String id;
+	private String title;
+	private String content;
+	private String file;
+	private String day;
+
+}
--- src/main/java/com/spring/mo/MoController.java
+++ src/main/java/com/spring/mo/MoController.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.spring.mapper.BoardVO;
+import com.spring.mapper.CommentVO;
 import com.spring.mapper.MemberMapper;
 import com.spring.mapper.MemberVO;
 
@@ -43,22 +46,57 @@ public class MoController {
 
 	// 로그인 시도
 	@RequestMapping("/loginTry.do")
-	public String loginTry(@RequestParam HashMap<String, String> paramMap) {
-		String id = paramMap.get("id");
-		String pw = paramMap.get("pw");
+	public String loginTry(@RequestParam("id") String id, @RequestParam("pw") String pw, Model model) {
 
 		MemberVO vo = memberMapper.loginTry(id);
-		System.out.println(vo.getId());
 
 		if (pw.equals(vo.getPw())) {
+			// 로그인성공 --> 회원정보 전달
+			model.addAttribute("vo", vo);
 			return "logmain";
 		} else {
+			// 로그인실패 --> 로그인창으로 다시 go
 			return "redirect:/login.do";
 		}
 	}
 
+	// 게시글 전체 목록 보기
 	@RequestMapping("/community.do")
-	public String community() {
+	public String community(Model model) {
+		//List<BoardVO> list = memberMapper.community();
+		//model.addAttribute("list", list);
 		return "community";
 	}
+	
+	// 게시글 하나 보기 + 댓글보기
+	@RequestMapping("/community_view.do")
+	public String community_view(int no, Model model) {
+		BoardVO vo = memberMapper.community_view(no); // 게시글
+		List<CommentVO> list = memberMapper.viewComment(no); // 댓글
+		model.addAttribute("vo", vo);
+		model.addAttribute("list", list);
+		return "community_view";
+	}
+	
+	// 댓글입력
+	@RequestMapping("/writeComment.do")
+	public String writeComment(int no) {
+		memberMapper.writeComment(no);
+		return "redirect:/community_view.do";
+	}
+	
+	// 게시글 입력 페이지 보기
+	@RequestMapping("/writeBoard.do")
+	public String writeBoard(String id, Model model) {
+		model.addAttribute("id", id);
+		return "writeBoard";
+	}
+	
+	// 게시글 입력 기능
+	@RequestMapping("/insertBoard.do")
+	public String insertBoard(BoardVO vo) {
+		memberMapper.insertBoard(vo);
+		return "redirect:/community.do";
+	}
+	
 }
